b25afd745f632d8a07e17c6f59744ed09e565d89
ayan-de <deayan@gmail.com>
2025-07-21 17:43:37 +0530

    user file setup completed

--- apps/users/src/users.controller.ts
+++ apps/users/src/users.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Controller()
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Get()
-  getHello(): string {
-    return this.usersService.getHello();
+  //message pattern - domainName.operationName
+  @MessagePattern('users.findAll')
+  findAll() {
+    return this.usersService.findAll();
   }
 }
